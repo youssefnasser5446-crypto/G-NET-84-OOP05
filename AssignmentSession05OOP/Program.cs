e5e42b55dbd2ac9b9b498a0ccc085bb4ae9b16dd
youssefnasser5446-crypto <youssef.nasser5446@gmail.com>
2026-08-15 02:36:24 +0300

    Question01

--- AssignmentSession05OOP/Program.cs
+++ AssignmentSession05OOP/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+
+            /*
+             a) What happens when you assign one object variable to another object variable?
+
+            ans => Changes to the fields will be reflected in both, because they both point to the same object.
+
+            b) Does assigning one object to another create a new object? Explain.
+
+            ans => No. It makes both variables point to the same object;
+            a modification through one variable will be reflected when accessing the object through the other.
+
+            c) What is the difference between copying an object and copying its reference?
+
+            ans => Copying an object means creating a new object on the heap and copying the data into it;
+            modifications to one do not affect the other. However, 
+            copying its reference results in the new reference pointing to the same object,
+            so modifications to one are reflected in the other.
+
+             */
+
+            #endregion
         }
     }
 }
