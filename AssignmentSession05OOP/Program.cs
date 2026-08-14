a70ec0fe7f7b2cce5f678039c231c7f6bd98364a
youssefnasser5446-crypto <youssef.nasser5446@gmail.com>
2026-08-15 01:41:26 +0300

    Add project files.

---
+++ AssignmentSession05OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AssignmentSession05OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
