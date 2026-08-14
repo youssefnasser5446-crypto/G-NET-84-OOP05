afe0eb96dd26f5410e298a4f76b93f3d77c1564b
youssefnasser5446-crypto <youssef.nasser5446@gmail.com>
2026-08-15 02:56:02 +0300

    Question02

--- AssignmentSession05OOP/Program.cs
+++ AssignmentSession05OOP/Program.cs
@@ -26,6 +26,37 @@
              */
 
             #endregion
+
+            #region Question02
+
+            /*
+              a) What is a Shallow Copy?
+
+            ans => It creates a new object and copies the values of all fields. 
+            For value-type fields, the value itself is copied; for reference-type fields,
+            the reference is copied, so both objects point to the same referenced object.
+
+            b) What is a Deep Copy?
+
+            ans => It copies the object's internal value-type data,
+            but for internal references—such as nested objects—it creates a new object on the heap.
+
+            c) What happens to reference-type members when a Shallow Copy is created?
+
+            ans => It copies the address and points to the same object—except for strings, because they are immutable.
+
+            d) What happens to reference-type members when a Deep Copy is created?
+
+            ans => It creates a new object for this reference, making it independent of the original one.
+
+            e) Give one situation where Deep Copy would be safer than Shallow Copy
+
+            ans => When two objects need to be modified independently, 
+            Deep Copy is safer because changes made to one object will not affect the other.
+
+             */
+
+            #endregion
         }
     }
 }
